bcd76be22a42d8fa6a0c5c49c0f5da7ed6bbd767
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2015-04-23 19:07:45 +0200

    Cooler animations

--- WearExamples/wear/src/main/java/no/bekk/wearexamples/MyListAdapter.java
+++ WearExamples/wear/src/main/java/no/bekk/wearexamples/MyListAdapter.java
@@ -1,14 +1,15 @@
 package no.bekk.wearexamples;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.wearable.view.CircledImageView;
 import android.support.wearable.view.WearableListView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -24,7 +25,14 @@ public class MyListAdapter extends WearableListView.Adapter {
 
     @Override
     public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new MyViewHolder(new ItemView(context));
+        final ItemView itemView = new ItemView(context);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Clicked " , Toast.LENGTH_SHORT).show();
+            }
+        });
+        return new MyViewHolder(itemView);
     }
 
     @Override
@@ -46,26 +54,76 @@ public class MyListAdapter extends WearableListView.Adapter {
     }
 
     private final class ItemView extends FrameLayout implements WearableListView.OnCenterProximityListener {
-        public TextView itemContentView;
-        public CircledImageView imageView;
+        private static final int ANIMATION_DURATION_MS = 150;
+        private static final float SHRINK_CIRCLE_RATIO = .80f;
+        private static final float SHRINK_LABEL_ALPHA = .5f;
+        private static final float EXPAND_LABEL_ALPHA = 1f;
+
+        public final TextView itemContentView;
+        public final CircledImageView imageView;
+        private final ObjectAnimator expandCircleAnimator;
+        private final ObjectAnimator expandLabelAnimator;
+        private final AnimatorSet expandAnimator;
+
+        private final float expandCircleRadius;
+        private final float shrinkCircleRadius;
+        private final ObjectAnimator shrinkCircleAnimator;
+        private final ObjectAnimator shrinkLabelAnimator;
+        private final AnimatorSet shrinkAnimator;
 
         public ItemView(Context context) {
             super(context);
             View.inflate(context, R.layout.todo_item_layout, this);
             this.itemContentView = (TextView) findViewById(R.id.text);
             this.imageView = (CircledImageView) findViewById(R.id.image);
+            expandCircleRadius = imageView.getCircleRadius();
+            shrinkCircleRadius = expandCircleRadius * SHRINK_CIRCLE_RATIO;
+
+            shrinkCircleAnimator = ObjectAnimator.ofFloat(imageView, "circleRadius",
+                    expandCircleRadius, shrinkCircleRadius);
+            shrinkLabelAnimator = ObjectAnimator.ofFloat(itemContentView, "alpha",
+                    EXPAND_LABEL_ALPHA, SHRINK_LABEL_ALPHA);
+            shrinkAnimator = new AnimatorSet().setDuration(ANIMATION_DURATION_MS);
+            shrinkAnimator.playTogether(shrinkCircleAnimator, shrinkLabelAnimator);
+
+            expandCircleAnimator = ObjectAnimator.ofFloat(imageView, "circleRadius",
+                    shrinkCircleRadius, expandCircleRadius);
+            expandLabelAnimator = ObjectAnimator.ofFloat(itemContentView, "alpha",
+                    SHRINK_LABEL_ALPHA, EXPAND_LABEL_ALPHA);
+            expandAnimator = new AnimatorSet().setDuration(ANIMATION_DURATION_MS);
+            expandAnimator.playTogether(expandCircleAnimator, expandLabelAnimator);
         }
 
         @Override
-        public void onCenterPosition(boolean b) {
-            itemContentView.animate().scaleX(1f).scaleY(1f).alpha(1);
-            imageView.animate().scaleX(1f).scaleY(1f).alpha(1);
+        public void onCenterPosition(boolean animate) {
+            if (animate) {
+                shrinkAnimator.cancel();
+                if (!expandAnimator.isRunning()) {
+                    expandCircleAnimator.setFloatValues(imageView.getCircleRadius(), expandCircleRadius);
+                    expandLabelAnimator.setFloatValues(itemContentView.getAlpha(), EXPAND_LABEL_ALPHA);
+                    expandAnimator.start();
+                }
+            } else {
+                expandAnimator.cancel();
+                imageView.setCircleRadius(expandCircleRadius);
+                itemContentView.setAlpha(EXPAND_LABEL_ALPHA);
+            }
         }
 
         @Override
-        public void onNonCenterPosition(boolean b) {
-            itemContentView.animate().scaleX(0.8f).scaleY(0.8f).alpha(0.6f);
-            imageView.animate().scaleX(0.8f).scaleY(0.8f).alpha(0.6f);
+        public void onNonCenterPosition(boolean animate) {
+            if (animate) {
+                expandAnimator.cancel();
+                if (!shrinkAnimator.isRunning()) {
+                    shrinkCircleAnimator.setFloatValues(imageView.getCircleRadius(), shrinkCircleRadius);
+                    shrinkLabelAnimator.setFloatValues(itemContentView.getAlpha(), SHRINK_LABEL_ALPHA);
+                    shrinkAnimator.start();
+                }
+            } else {
+                shrinkAnimator.cancel();
+                imageView.setCircleRadius(shrinkCircleRadius);
+                itemContentView.setAlpha(SHRINK_LABEL_ALPHA);
+            }
         }
     }
 }
